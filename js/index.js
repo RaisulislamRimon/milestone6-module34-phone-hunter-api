0d7e86ee93ee4024b5eb54de126d3ca08aa20b24
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-29 23:44:18 +0600

    using async await

--- js/index.js
+++ js/index.js
@@ -1,21 +1,8 @@
-const loadPhone = () => {
+const loadPhones = async () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
-  fetch(url)
-    .then((response) => response.json())
-    .then((data) => {
-      displayPhone(data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
 };
 
-const displayPhone = (data) => {
-  console.log(data.data);
-  const phone = data.data;
-  phone.forEach((phn) => {
-    console.log(phn.brand);
-  });
-};
-
-loadPhone();
+loadPhones();
