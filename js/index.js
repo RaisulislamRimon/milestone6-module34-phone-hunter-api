eae26ddd166a9c2b6443e54cb2cfb061812ab45c
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-30 00:11:03 +0600

    34-1 Load data using Phone Hunter API

--- js/index.js
+++ js/index.js
@@ -2,7 +2,15 @@ const loadPhones = async () => {
   const url = `https://openapi.programming-hero.com/api/phones?search=iphone`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
+  displayPhones(data.data);
+};
+
+const displayPhones = (phones) => {
+  console.log(phones);
+  const phoneContainer = document.getElementById("phone-container");
+  phones.forEach((phone) => {
+    console.log(phone);
+  });
 };
 
 loadPhones();
