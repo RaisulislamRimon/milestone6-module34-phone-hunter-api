3dac4e8382a467adba5d2aa68303498bb87b1d6e
RaisulislamRimon <rimonppip0@gmail.com>
2022-08-30 14:12:18 +0600

    34-3 Toggle no phone found message based on search result

--- module34-3/js/index.js
+++ module34-3/js/index.js
@@ -8,15 +8,28 @@ const loadPhones = async (phone) => {
 const displayPhones = (phones) => {
   // console.log(phones);
   const phoneContainer = document.getElementById("phone-container");
-  phones = phones.slice(0, 5);
+  // display limited phones
+  phones = phones.slice(0, 10);
   // phoneContainer.innerHTML = "";
   phoneContainer.textContent = "";
+
+  // display no phone found
+  if (phones.length === 0) {
+    const noPhoneFound = document.getElementById("no-phone-found");
+    noPhoneFound.classList.remove("d-none");
+  }
+  // we can hide/display none simply by adding/removing the class d-none to the element in html file: line no: 54
+  else {
+    const noPhoneFound = document.getElementById("no-phone-found");
+    noPhoneFound.classList.add("d-none");
+  }
+  // display all phone found
   phones.forEach((phone) => {
     const phoneDiv = document.createElement("div");
     phoneDiv.classList.add("col");
     phoneDiv.innerHTML = `
       <div class="card m-2 p-3 bg-dark text-white border  ">
-        <img src="${phone.image}" class="card-img-top" alt="no image found" />
+        <img src="${phone.image}" class="card-img-top rounded" alt="no image found" />
         <div class="card-body">
           <h5 class="card-title">${phone.phone_name}</h5>
           <p class="card-text">${phone.brand}</p>
